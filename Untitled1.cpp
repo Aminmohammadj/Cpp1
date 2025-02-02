53213e2beb08e9b46c7dd987de3dccd1a47fc55a
Aminmohammadj <aminmohammadjavaher0017@gmail.com>
2025-02-02 16:11:50 +0330

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Helo Amin" << endl;
+	for (int i = 0; i <= 10; i++)
+	{
+		for (int j = 0; j <= i; j++)
+			cout << "*" << " ";
+		cout << endl;
+	}
+	cout << "Amin\n";
+	cout << "/-/-/-/-/" << endl;
+	cout << "\n\n\n";
+	int a = 20;
+	int b = 19;
+	cout << "Amin mohammd beglari";
+	int ad = 22;
+	cout << "Adfami";d
+}
