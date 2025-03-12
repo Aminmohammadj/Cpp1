71bd5b056e4f0e332967156ffa6cf9a5b12d56eb
Aminmohammadj <aminmohammadjavaher0017@gmail.com>
2025-03-12 02:24:40 -0700

    EZAFE KARDAN DD BI AKHAR JOMLE

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	cout << "Helo Amin mohammad javaher" << endl;
+	cout << "Helo Amin mohammad javaher DD" << endl;
 	for (int i = 0; i <= 10; i++)
 	{
 		for (int j = 0; j <= i; j++)
